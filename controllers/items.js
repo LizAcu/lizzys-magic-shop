b69107194a7088f93a954a7c12fb84f52d62a699
Lisette Acuria <lacuria965@gmail.com>
2020-10-29 09:53:38 -0400

    added more items to index and styled headers

--- controllers/items.js
+++ controllers/items.js
@@ -46,21 +46,27 @@ router.get('/items', (req, res) => {
 	{
 		name: 'Magic Staff',
 		color: 'wooden',
-		description: 'making lots of things happen.',
+		description: 'levitating and making lots of things happen!',
 		image: 'https://i.pinimg.com/originals/9a/04/a8/9a04a8ad79de4ad75ccfe0976d8106fc.jpg'
 
 	},
 	{
 		name: 'Magic Hat',
 		color: 'no color',
-		description: 'pulling out desserts like you would a rabbit!',
+		description: 'pulling out desserts like you would a rabbit! WARNING: your dessert might bite back.',
 		image: 'https://www.laphamsquarterly.org/sites/default/files/images/roundtable/hatmain_0.jpg'
 	},
 	{
 		name: 'Spicy runes',
 		color: 'wooden', 
-		description: 'alphabetizing your life',
+		description: 'alphabetizing your life! YES we mean literally!',
 		image: 'https://i.pinimg.com/originals/a4/64/b3/a464b3acca99b7bb5b000a0aab4475de.jpg'
+	},
+	{
+		name: 'Galaxy in a jar',
+		color: 'starry',
+		description: 'diving in and exploring the this tiny galaxy, yes, you CAN jump in!',
+		image: 'https://storage.googleapis.com/little-passports-blog/hubble_friday_08072015.jpg'
 	}
 	
 
