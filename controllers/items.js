d5fd2f57354162d1e9162670f47eed2a25a96f6f
Lisette Acuria <lacuria965@gmail.com>
2020-10-29 12:06:28 -0400

    added image to add page

--- controllers/items.js
+++ controllers/items.js
@@ -38,19 +38,6 @@ router.get('/', (req, res)=>{
 router.get('/items', (req, res) => {
 	Items.create([
 	{
-		name: 'Disappearing Robe',
-		color: 'red',
-		description: 'making you go poof (WHERE you go may vary)!',
-		image: 'https://www.picclickimg.com/d/l400/pict/223552126732_/Black-Hooded-Velvet-Wizard-Cloak-Cape-Men-Halloween-With.jpg'
-	},
-	{
-		name: 'Magic Staff',
-		color: 'wooden',
-		description: 'levitating, and making lots of things happen!',
-		image: 'https://i.pinimg.com/originals/9a/04/a8/9a04a8ad79de4ad75ccfe0976d8106fc.jpg'
-
-	},
-	{
 		name: 'Magic Hat',
 		color: 'no color',
 		description: 'pulling out desserts like you would a rabbit! WARNING: your dessert might bite back.',
@@ -67,6 +54,26 @@ router.get('/items', (req, res) => {
 		color: 'starry',
 		description: 'diving in and exploring this tiny galaxy, yes, you CAN jump in!',
 		image: 'https://storage.googleapis.com/little-passports-blog/hubble_friday_08072015.jpg'
+	},
+	{
+		name: 'Travel stones',
+		color: 'all color',
+		description: 'quickly getting you places!',
+		image: 'https://images.squarespace-cdn.com/content/v1/5beb0a44f2e6b1113f9519d9/1564200955420-1ZYCBODQ5IU2KY76M99U/ke17ZwdGBToddI8pDm48kIMgItuLfrBfs8_Vz6fY_v9Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIOoLtpHiREIxpGqLX9yLlbPF78fv36IoADR8RM4bC6WYKMshLAGzx4R3EDFOm1kBS/Stones+for+travel.jpg'
+	},
+	{
+		name: 'Magic Staff',
+		color: 'wooden',
+		description: 'levitating, and making lots of things happen!',
+		image: 'https://i.pinimg.com/originals/9a/04/a8/9a04a8ad79de4ad75ccfe0976d8106fc.jpg'
+
+	},
+	
+	{
+		name: 'Disappearing Robe',
+		color: 'red',
+		description: 'making you go poof (WHERE you go may vary)!',
+		image: 'https://www.picclickimg.com/d/l400/pict/223552126732_/Black-Hooded-Velvet-Wizard-Cloak-Cape-Men-Halloween-With.jpg'
 	}
 	
 
